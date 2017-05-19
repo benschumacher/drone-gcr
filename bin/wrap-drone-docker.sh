2d7c42426fdb2d6baae132641f2e17694ae7f909
Ben Schumacher <bschumac@cisco.com>
2017-05-19 11:13:14 -0600

    Simplier wrap script

--- bin/wrap-drone-docker.sh
+++ bin/wrap-drone-docker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
 
 if [ "${PLUGIN_DEBUG:-}" = "true" ]; then
@@ -6,25 +6,23 @@ if [ "${PLUGIN_DEBUG:-}" = "true" ]; then
 fi
 
 if [ -n "$PLUGIN_AUTH_KEY" ]; then
-    export GCR_AUTH_KEY="${PLUGIN_AUTH_KEY}"
+    export GCR_AUTH_KEY
+    if (echo "${PLUGIN_AUTH_KEY}" | jq -e '.'); then
+        GCR_AUTH_KEY="${PLUGIN_AUTH_KEY}"
+    else
+        GCR_AUTH_KEY="$(echo "${PLUGIN_AUTH_KEY}" | base64 -d)"
+    fi
 fi
 
-#TMPFILE="$(mktemp -d)/keyfile.json"
 TMPFILE=$(mktemp)
-echo "${GCR_AUTH_KEY}" | base64 -d > "${TMPFILE}"
+echo "${GCR_AUTH_KEY}" > "${TMPFILE}"
 GCR_ACCOUNT=$(jq -r ".client_email" "${TMPFILE}")
 
-# prepare these values for the drone-docker plugin
-export DOCKER_REGISTRY DOCKER_USERNAME DOCKER_PASSWORD DOCKER_EMAIL
+# set variables for using Docker with GCR
+export DOCKER_REGISTRY DOCKER_USERNAME DOCKER_PASSWORD
 DOCKER_REGISTRY="${PLUGIN_REGISTRY:-gcr.io}"
 DOCKER_USERNAME="_json_key"
-DOCKER_PASSWORD="$(cat ${TMPFILE})"
-
-# use debug?
-export PLUGIN_DEBUG
-PLUGIN_DEBUG=true
-
-env
+DOCKER_PASSWORD="${GCR_AUTH_KEY}"
 
 # invoke the docker plugin
 exec /bin/drone-docker "$@"
